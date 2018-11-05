4695fb05d25cb20c771e0c7d5863b96687338f28
Christopher Benedict <chrisjbenedict@gmail.com>
2018-11-05 17:09:50 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+# sort of similar to bachelor lab
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  # creat an empty hash 'pigeon_list'
+  pigeon_list = {}
+  # iterate over the data and for each attribute
+  data.each do |attribute, choices|
+    # iterate over each choice
+    choices.each do |choice, names|
+      #iterate over each pigeon name within each choice
+      names.each do |name|
+        pigeon_list[name]
+        pigeon_list[name][attribute]
+        pigeon_list[name][attribute] << choice.to_s
+      end
+    end
+  end
+  pigeon_list
 end
